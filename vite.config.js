d46135d01fe2bbf343d44a0b33a92a4532e85399
vincent cupid tabiongan <cupidvincent@gmail.com>
2023-07-17 22:14:34 +0800

    chnage vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+export default defineConfig(({ mode }) => {
+  let proxyURL;
+
+  if (mode === 'development') {
+    proxyURL = 'http://localhost:5000'; // Proxy URL for development environment
+  } else {
+    proxyURL = 'https://tic-tac-toe-h8ml.onrender.com'; // Proxy URL for production environment
   }
-})
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: proxyURL,
+          changeOrigin: true
+        },
+      },
+    },
+  };
+});
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
